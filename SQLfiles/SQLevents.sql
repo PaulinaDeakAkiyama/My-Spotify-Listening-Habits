c0ee69c01f3ebece149a20d694781dbef9ab0726
PaulinaDeakAkiyama <paulinadeak@hotmail.com>
2025-11-20 13:57:23 +0000

    updated sql files

--- SQLfiles/SQLevents.sql
+++ SQLfiles/SQLevents.sql
@@ -1,6 +1,11 @@
+SET GLOBAL event_scheduler = ON;
+USE myspotify;
 DROP EVENT IF EXISTS delete_filler_dates;
 
 CREATE EVENT IF NOT EXISTS delete_filler_dates
 ON SCHEDULE EVERY 1 HOUR
 DO
-  CALL clean_filler_dates();
+  CALL delete_filler_dates();
+  
+SHOW EVENTS;
+  
