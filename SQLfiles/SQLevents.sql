9e21545ed5eb92c8d9186e13de1aac782334e4dd
PaulinaDeakAkiyama <paulinadeak@gmail.com>
2026-01-25 10:29:41 +0000

    Change scheduled event to call clean_and_backup_listening

--- SQLfiles/SQLevents.sql
+++ SQLfiles/SQLevents.sql
@@ -5,7 +5,7 @@ DROP EVENT IF EXISTS delete_filler_dates;
 CREATE EVENT IF NOT EXISTS delete_filler_dates
 ON SCHEDULE EVERY 1 HOUR
 DO
-  CALL delete_filler_dates();
+  CALL clean_and_backup_listening();
   
 SHOW EVENTS;
   
